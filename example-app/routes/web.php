577e16276f64e6670a5cbc66a722860bcb7e4bf1
Boya0828 <kidohagai@gmail.com>
2024-02-12 18:48:22 +0700

    1

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
-use App\Http\Controllers\Mycontroller;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Http;
+use App\Http\Controllers\C_titles;
+use App\Http\Controllers\MyAuth;
+use App\Http\Controllers\MyController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,26 +15,42 @@ use Illuminate\Http\Request;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-|
-*/
-//Route::get('/mycontroller',[Mycontroller::class,'index'] );
-//Route::get('/mycontroller2', 'App\Http\Controllers\Mycontroller');
+|*/
 
-Route::get('/my-route', function () {
+Route::get('/login', [MyAuth::class, 'login_view'])->name('login');
+Route::get('/register', [MyAuth::class, 'register_view']);
+Route::get('/logout', [MyAuth::class, 'logout_process']);
+Route::post('/login', [MyAuth::class, 'login_process']);
+Route::post('/register', [MyAuth::class, 'register_process']);
 
-    return view('my-route');
+Route::resource('titles', C_titles::class)->middleware('auth');
+Route::middleware('auth')->group(function(){
+    // auth first
 });
-Route::get('/admin', function () {
 
-    return view('Admin101');
+Route::get('/my-controller2', 'App\Http\Controllers\MyController@index');
+Route::namespace('App\Http\Controllers')->group(function(){
+    Route::get('/my-controller3', 'MyController@index');
+    Route::post('/my-controller3-post', 'MyController@store');
 });
-/*
-Route::post('/my-route',function(Request $req){
-    $data['numberinput'] = $req->input('numberinput');
-    return view('newroute',$data);
 
+Route::resource('/my-controller4', MyController::class);
+
+
+Route::get('/', function () {
+    return view('welcome'); // welcome.blade.php
 });
-*/
 
 
-Route::post('/mail',[Mycontroller::class,'store']);
+Route::get('/my-route', function(){
+    // return view('myroute');
+    //        Key    =>  Value
+    $data = ['val_a' => 'Hello World!'];
+    $data['val_b'] = "Laravel";
+    return view('myfolder.mypage',$data);});
+
+
+Route::post('/my-route', function(Request $req){
+    $data['myinput'] =  $req->input('myinput');
+    return view('myroute', $data);
+});
